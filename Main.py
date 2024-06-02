bdd31caa4dbe9ff6aed0aabaf9ef17877b0f6761
RealmDragon <realmdragon2014@yandex.ru>
2024-06-02 18:40:12 +0300

    0.1 добавлена функция маскеровки номера карты 0.2 добавлена функция форматирования данных об операции для вывода

--- Main.py
+++ Main.py
@@ -7,3 +7,19 @@ def mask_card_number(card_number):
     return f"XXXX XX** **** {card_number[-4:]}"
 
 
+def format_operation(operation):
+    """Форматирует данные об операции для вывода."""
+    date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%d.%m.%Y')
+    amount, currency = operation['operationAmount']['amount'], operation['operationAmount']['currency']
+
+    if 'from' in operation:
+        from_value = mask_card_number(operation['from']) if 'card' in operation['from'] else mask_account_number(
+            operation['from'])
+    else:
+        from_value = 'Неизвестно'
+
+    to_value = mask_card_number(operation['to']) if 'card' in operation['to'] else mask_account_number(operation['to'])
+
+    return f"""{date} {operation['description']}
+{from_value} -> {to_value}
+{amount} {currency}"""
