c52142e1bb99d913aa8ea054312e5c0b1b6c39bb
RealmDragon <realmdragon2014@yandex.ru>
2024-06-03 11:37:39 +0300

    0.1 добавлена функция маскеровки номера карты 0.2 добавлена функция форматирования данных об операции для вывода 0.3 добавлена функция списков из 5  последних операций 0.4 Проведина работа над ошибками ?

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime
 
-
 def mask_card_number(card_number):
     """Маскирует номер карты."""
     return f"XXXX XX** **** {card_number[-4:]}"
@@ -12,8 +11,8 @@ def mask_account_number(account_number):
 
 def format_operation(operation):
     """Форматирует данные об операции для вывода."""
-    date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%d.%m.%Y')
-    amount, currency = operation['operationAmount']['amount'], operation['operationAmount']['currency']
+    date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d.%m.%Y')
+    amount, currency = operation['operationAmount']['amount'], operation['operationAmount']['currency']['code']
 
     if 'from' in operation:
         from_value = mask_card_number(operation['from']) if 'card' in operation['from'] else mask_account_number(
@@ -27,23 +26,20 @@ def format_operation(operation):
 {from_value} -> {to_value}
 {amount} {currency}"""
 
-def get_last_operations(filename='operations.json'):
+def get_last_operations(operations):
     """Возвращает список из 5 последних выполненных операций."""
-    with open(filename, 'r', encoding='utf-8') as f:
-        operations = json.load(f)
-
     executed_operations = []
     for op in operations:
-        try:
-            if op['state'] == 'EXECUTED':
-                executed_operations.append(op)
-        except KeyError:
-            print(f"Ошибка: Отсутствует ключ 'state' в операции {op['id']}")
+        if op['state'] == 'EXECUTED':
+            executed_operations.append(op)
 
     executed_operations.sort(key=lambda x: x['date'], reverse=True)
     return [format_operation(op) for op in executed_operations[:5]]
 
 if __name__ == '__main__':
-    for operation in get_last_operations():
+    operations = [
+        # ... (Your operations data)
+    ]
+    for operation in get_last_operations(operations):
         print(operation)
         print()
